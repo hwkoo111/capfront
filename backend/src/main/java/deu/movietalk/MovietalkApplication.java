b432b75c82cb5d382b74c11007e060d0e9c12913
hwkoo111 <qkrrn0315@naver.com>
2025-05-13 22:01:42 +0900

    chore: add backend folder into capfront repo

---
+++ backend/src/main/java/deu/movietalk/MovietalkApplication.java
@@ -0,0 +1,14 @@
+package deu.movietalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovietalkApplication {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+		SpringApplication.run(MovietalkApplication.class, args);
+	}
+
+}
